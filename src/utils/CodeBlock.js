68e46bae8f271c87ee4c0725bf19de5de3842dd4
samuelwigren <samuel.wigren@gsfacket.se>
2023-10-25 16:39:57 +0200

    Add utils.js, CodeBlock.js and CopyTextBlock.js and styles module

---
+++ src/utils/CodeBlock.js
@@ -0,0 +1,26 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+
+const CodeBlock: React.FC<{ codestring: string }> = ({ codestring }) => {
+  let codeWithMinLines = codestring;
+  if (codestring) { 
+  const minLines = 2; // Adjust the minimum number of lines as needed
+
+  const codeLines = codestring.split('\n');
+  const emptyLines = new Array(Math.max(minLines - codeLines.length, 0)).fill('');
+
+  codeWithMinLines = codeLines.concat(emptyLines).join('\n');
+  }
+return (
+  <SyntaxHighlighter
+    // language="javascript"
+    style={a11yDark}
+    showLineNumbers>
+    {codeWithMinLines}
+     {/*{codestring}*/}
+  </SyntaxHighlighter>
+);
+};
+
+export default CodeBlock;
