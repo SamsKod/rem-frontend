928de8f5021b4970f041b562ef7d6256b008c49b
samuelwigren <samuel.wigren@gsfacket.se>
2023-10-25 12:47:24 +0200

    Add logo.png, empty.png and navbar image

---
+++ src/components/Asset.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+
+
+const Asset = ({ spinner, src, message }) => {
+  return (
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && <Spinner 
+          as="span"
+          animation="grow"
+          size="lg"
+          role="status"
+          aria-hidden="true" />}
+      {src && <img src={src} alt={message} />}
+      {message && <p className="mt-4">{message}</p>}
+    </div>
+  );
+};
+
+export default Asset;
